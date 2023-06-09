b5c7789af37c8affa7ae00b1cd7fdd9f481d8fce
Aziyev <idrisaa@code.edu.az>
2023-06-09 20:29:28 +0400

    hgtgtdf

---
+++ AydansProjectsPractice/Website1 Responsive/js/app.js
@@ -0,0 +1,9 @@
+/* Mobile Header Open Bar */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+  
